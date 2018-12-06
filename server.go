d917cf13e3b01fcf253678427f2c0407d4770110
MoD <mod@websys.io>
2018-12-06 22:51:05 +0200

    Adding flags

--- server.go
+++ server.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+  "flag"
   "log"
 
   "github.com/valyala/fasthttp"
 )
 
 var (
-  filesHandler = fasthttp.FSHandler("build", 1)
+  addr  = flag.String("h", "localhost:8080", "TCP address to listen to")
+  root  = flag.String("d", "/usr/share/httpd", "Directory to serve static files from")
+  strip = flag.Int("s", 0, "Number of mount point to skip")
+  // compress  = flag.Bool("compress", false, "Enables transparent response compression if set to true")
 )
 
-// Main request handler
-func requestHandler(ctx *fasthttp.RequestCtx) {
-  switch {
-
-  default:
-    filesHandler(ctx)
-  }
-}
-
 func main() {
-  if err := fasthttp.ListenAndServe(":8080", requestHandler); err != nil {
-    log.Fatalf("Error in server: %s", err)
+  flag.Parse()
+
+  fsHandler := fasthttp.FSHandler(*root, *strip)
+  requestHandler := func(ctx *fasthttp.RequestCtx) {
+    fsHandler(ctx)
+  }
+
+  // Start HTTP server.
+  if len(*addr) > 0 {
+    log.Printf("Starting HTTP server on %q", *addr)
+    log.Printf("Serving files from directory %q", *root)
+    if err := fasthttp.ListenAndServe(*addr, requestHandler); err != nil {
+      log.Fatalf("error in ListenAndServe: %s", err)
+    }
   }
 }
