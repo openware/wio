357a48fc73849a811b3ab47702a229d4f3906cd3
MoD <mod@websys.io>
2018-12-07 02:11:01 +0200

    Adding license and formatting go code

--- server.go
+++ server.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-  "flag"
-  "log"
+	"flag"
+	"log"
 
-  "github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp"
 )
 
 var (
-  addr  = flag.String("h", "0.0.0.0:8080", "TCP address to listen to")
-  root  = flag.String("d", "/usr/share/httpd", "Directory to serve static files from")
-  strip = flag.Int("s", 0, "Number of mount point to skip")
-  compress = flag.Bool("c", false, "Enables transparent response compression if set to true")
-  fsHandler fasthttp.RequestHandler
+	addr      = flag.String("h", "0.0.0.0:8080", "TCP address to listen to")
+	root      = flag.String("d", "/usr/share/httpd", "Directory to serve static files from")
+	strip     = flag.Int("s", 0, "Number of mount point to skip")
+	compress  = flag.Bool("c", false, "Enables transparent response compression if set to true")
+	fsHandler fasthttp.RequestHandler
 )
 
 func notFoundHandler(ctx *fasthttp.RequestCtx) {
-  ctx.Logger().Printf("File %s not found, defaulting to index.html", ctx.Path())
-  ctx.Request.SetRequestURI("/index.html")
-  fsHandler(ctx)
+	ctx.Logger().Printf("File %s not found, defaulting to index.html", ctx.Path())
+	ctx.Request.SetRequestURI("/index.html")
+	fsHandler(ctx)
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
-  fsHandler(ctx)
+	fsHandler(ctx)
 }
 
 func createFsHandler(stripSlashes int) fasthttp.RequestHandler {
-  fs := &fasthttp.FS{
+	fs := &fasthttp.FS{
 		Root:               *root,
 		Compress:           *compress,
 		IndexNames:         []string{"index.html"},
-    PathNotFound:       notFoundHandler,
-    GenerateIndexPages: false,
-    AcceptByteRange:    true,
+		PathNotFound:       notFoundHandler,
+		GenerateIndexPages: false,
+		AcceptByteRange:    true,
 	}
-  if stripSlashes > 0 {
+	if stripSlashes > 0 {
 		fs.PathRewrite = fasthttp.NewPathSlashesStripper(stripSlashes)
 	}
-  return fs.NewRequestHandler()
+	return fs.NewRequestHandler()
 }
 
 func main() {
-  flag.Parse()
-  fsHandler = createFsHandler(*strip)
+	flag.Parse()
+	fsHandler = createFsHandler(*strip)
 
-  // Start HTTP server.
-  if len(*addr) > 0 {
-    log.Printf("Starting HTTP server on %q", *addr)
-    log.Printf("Serving files from directory %q", *root)
-    if err := fasthttp.ListenAndServe(*addr, requestHandler); err != nil {
-      log.Fatalf("error in ListenAndServe: %s", err)
-    }
-  }
+	// Start HTTP server.
+	if len(*addr) > 0 {
+		log.Printf("Starting HTTP server on %q", *addr)
+		log.Printf("Serving files from directory %q", *root)
+		if err := fasthttp.ListenAndServe(*addr, requestHandler); err != nil {
+			log.Fatalf("error in ListenAndServe: %s", err)
+		}
+	}
 }
